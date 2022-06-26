b12567dc92d4dbe6b467e8387245b309e8cf0978
Divya <divyapulivarthi09@gmail.com>
2022-06-26 21:19:27 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import cafelogo from './cafeheader.jpg';
+import React from 'react';
+import {useState, useReducer} from "react";
+import { Button, View, Text } from 'react-native';
 import './App.css';
-
+//G:\Javascript\React Projects\food-order-app
 let soup="Soup";
 let cake="Cup cakes & Pastries";
 let iceCream= "Ice creams";
@@ -36,12 +39,43 @@ function MenuItems(props){
   </div>)
 }
 
+function CustomerSatisfaction(props){
+  const [emotion,setEmotion]= useState("");
+  return (
+    <div>
+      Customer Satisaction: {emotion}<br/>
+      <button onClick = {()=>setEmotion("Happy")}>Happy</button>&nbsp;
+      <button onClick = {()=>setEmotion("Sad")}>Sad</button>&nbsp;
+      <button onClick = {()=>setEmotion("Neutral")}>Neutral</button>
+    </div>
+  )
+}
+
+function OrderItems(){
+  const [checked,setChecked]= useReducer(
+    (checked)=> !checked,
+    false
+  );
+  return(
+    <div>
+      Want to order?
+      <input type="checkbox" value ={checked} onChange = {setChecked} />
+      <label>{checked ? "Yes": "No"}</label>
+    </div>
+  )
+}
+
+function NavigateToGetOrder(){
+  return(
+    <Button title='Order something' onClick={() =>{window.location.href='Order.html'}} />
+  )
+}
+
 function Footer(){
   return (<footer className='App-footer'>CopyRights to Joe</footer>)
 }
 
 function App() {
-
   return (
     <div className="App">
       <MainLogo name="Joe"/>
@@ -49,9 +83,9 @@ function App() {
       <MenuItems dishes={dishObjects}/>
       <OrderItems />
       <CustomerSatisfaction />
+      <NavigateToGetOrder />
       <Footer />
     </div>
   );
 }
-
 export default App;
